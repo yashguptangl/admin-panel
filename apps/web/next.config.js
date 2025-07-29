dc9a6e398f7713aa87b0c51a833533d25093feed
Yash Gupta <yguptangl@gmail.com>
2025-07-30 00:39:05 +0530

    image_policy

--- apps/web/next.config.js
+++ apps/web/next.config.js
@@ -4,11 +4,11 @@ const nextConfig = {
         remotePatterns: [
             {
                 protocol: 'https',
-                hostname: 's3.ap-south-1.amazonaws.com',
+                hostname: 'roomlocus.s3.ap-south-1.amazonaws.com',
                 pathname: '**', // Allow all paths under this domain
             },
         ],
-        domains : ['s3.ap-south-1.amazonaws.com'], // Add your allowed domains here
+        domains : ['roomlocus.s3.ap-south-1.amazonaws.com'], // Add your allowed domains here
     },
 };
 
